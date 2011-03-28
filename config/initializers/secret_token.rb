980bbb8c5321aeaf6cfe008e93f1cd0d946629cf
Zachary Cancio <zacharycancio@zachary-cancios-macbook-pro.local>
2011-03-27 20:34:52 -0400

    initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dukeclub::Application.config.secret_token = '08e5409e40997b87c87f910d66e211f4be6f6a5480f333c99a2a5ab29a8538b18e214f941f42f8a80871e3c97b1e9e5b6371c1b74853316993720af016ace215'
